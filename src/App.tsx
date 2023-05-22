5405e3aebde957e620b5826ce4fba7787ccbc970
한규진 <david0218@naver.com>
2023-05-22 18:51:44 +0900

    feat : fcm message

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from "react";
-import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
+import { requestPermission } from "./firebase-messaging-sw";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [token, setToken] = useState("");
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    requestPermission()
+      .then((fcmToken) => setToken(fcmToken!))
+      .catch(() => alert("토큰 발급에 실패했어요"));
+  }, []);
+
+  const registerToken = async (token: string) => {
+    console.log(token);
+    try {
+      const response = await axios.post(
+        `http://ec2-43-201-38-179.ap-northeast-2.compute.amazonaws.com:8080/v1/users/register/${token}`
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const updateToken = async (id: string, token: string) => {
+    try {
+      const response = await axios.post(
+        `http://ec2-43-201-38-179.ap-northeast-2.compute.amazonaws.com:8080/v1/users/${id}/token/${token}`
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +43,22 @@ function App() {
           <br />
           PWA test
         </p>
+        <button onClick={() => registerToken(token)} disabled={!token.length}>
+          회원가입
+        </button>
+        <div className="update">
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <button
+            onClick={() => updateToken(id, token)}
+            disabled={!token.length}
+          >
+            업데이트
+          </button>
+        </div>
       </header>
     </div>
   );
---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,52 @@
+// Import the functions you need from the SDKs you need
+import axios from "axios";
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebase);
+
+const requestPermission = async () => {
+  console.log("권한 요청 중...");
+
+  const permission = await Notification.requestPermission();
+  if (permission === "denied") {
+    console.log("알림 권한 허용 안됨");
+    return;
+  }
+
+  console.log("알림 권한이 허용됨");
+
+  const token = await getToken(messaging, {
+    vapidKey: process.env.REACT_APP_VAPID_KEY,
+  });
+
+  if (token) {
+    console.log("token: ", token);
+  } else {
+    console.log("Can not get Token");
+  }
+
+  onMessage(messaging, (payload) => {
+    console.log("메시지가 도착했습니다.", payload);
+    // ...
+  });
+
+  return token;
+};
+
+export { requestPermission };
